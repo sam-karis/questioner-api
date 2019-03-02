99c11606ea6a49a962420200ff6911b6de51ddd9
sam-karis <samkaris75@gmail.com>
2019-03-02 06:28:55 +0300

    add user auth serializers

---
+++ questioner/apps/helpers/validators.py
@@ -0,0 +1,19 @@
+import re
+from rest_framework import serializers
+
+
+class Validators(object):
+
+    def is_valid_password(self, password):
+        if not re.match(r'^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).*', password):  # noqa E501
+            raise serializers.ValidationError(
+                'Password must contain a number, capital letter and special charachter'  # noqa E501
+            )
+        return password
+
+    def is_valid_string(self, string, entity):
+        if re.match(r'^[0-9]+[A-Za-z0-9]]*$', string):
+            raise serializers.ValidationError(
+                f'{entity} cannot contain numbers or special characters only'
+            )
+        return string
