e20e1ac92daf0ee9a8fce18a5f13ec513ddc0021
sam-karis <samkaris75@gmail.com>
2019-03-07 03:56:40 +0300

    Add profile endpoints

--- questioner/apps/user/views.py
+++ questioner/apps/user/views.py
@@ -1,11 +1,17 @@
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from rest_framework import status
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
 # local imports
+from questioner.apps.user.models import Profile
 from questioner.apps.user.serializers import (
-    RegisterSerializer, LoginSerializer
-)
+    RegisterSerializer, LoginSerializer, GetProfileSerializer,
+    )
+from questioner.apps.helpers.permissions import IsOwnerOrReadOnly
 
 
 class RegisterUserAPIView(APIView):
@@ -17,7 +23,6 @@ class RegisterUserAPIView(APIView):
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        # import pdb; pdb.set_trace()
         serializer.save()
 
         message = {
@@ -39,3 +44,34 @@ class LoginUserAPIView(APIView):
         serializer.is_valid(raise_exception=True)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class SpecificUserProfileAPIView(RetrieveUpdateDestroyAPIView):
+    '''
+    Handle getting a profile for specific user
+    '''
+    queryset = Profile.objects.all()
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
+    serializer_class = GetProfileSerializer
+
+    def get_object(self):
+        try:
+            username = self.kwargs.get('username')
+            profile = Profile.objects.select_related('user').get(
+                user__username=username
+            )
+        except Profile.DoesNotExist:
+            raise NotFound(f'No profile related to {username} username')
+        else:
+            self.check_object_permissions(self.request, profile)
+            return profile
+
+
+class GetAllProfileAPIView(ListAPIView):
+    '''
+    Handle getting of user profiles
+    '''
+
+    permission_classes = (IsAuthenticated,)
+    serializer_class = GetProfileSerializer
+    queryset = Profile.objects.all()
